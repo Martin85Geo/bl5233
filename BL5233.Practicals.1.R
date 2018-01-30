aa39e057deae10f84396a3660acd4443c5f93fa5
dondealban <dondealban@gmail.com>
2018-01-30 11:48:51 +0800

    Saved scripts

--- BL5233.Practicals.1.R
+++ BL5233.Practicals.1.R
@@ -16,13 +16,14 @@ newVar <- seq(1,nrow(data),1)
 
 # Combine new vector with text data
 data2 <- mutate(data, newVar)
+# data2 < cbind(data1,newVar)
 
 # Write combined data into a text file
 write.table(data2, "data2.txt")
 
 # Calculate mean and standard deviation of second column
-mean(data2$Fruit)
-sd(data2$Fruit)
+mean(data2$Fruit, na.rm=T)
+sd(data2$Fruit, na.rm=T)
 
 # Subset new data
 subFruit <- data2[data2$Fruit < 50,]
@@ -37,4 +38,57 @@ Grazed <- data2[data2$Grazing=="Grazed", na.omit=TRUE] # Do this!
 meanGrazed <- mean(Grazed$Fruit)
 Ungrazed <- data2[data2$Grazing=="Ungrazed",]
 meanUngrazed <- mean(Ungrazed$Fruit, na.omit=TRUE) # Do this! 
+
+# 7. 
+by(data$Fruit, data2$Grazing, mean)
+
+# 8. 
+plot(data2$Root, data2$Fruit, xlab="root", ylab="fruit weight")
+
+# 9. 
+howManyNAs <- function(x){
+              apply(apply(x,2,is.na),2,sum)
+              }
+howManyNAs(data1)
+
+# 10. 
+scaledRoot <- scale(data2$Root, center=TRUE, scale=TRUE)
+
+# 11. 
+library(rpart)
+
+# 12. pnbinom(30, size=0.9, mu=10, lower.tail=FALSE)
+# 13. qnbinom(0.95, size=0.9, mu=10)
+#     qnbinom(c(0.025,0.975),size=0.9, mu=10)
+# 14. z <- rnbinom(1000, mu=10, size=0.9)
+max <- max(z)
+f <- factor(z, levels=0:maxz)
+
+# 15.
+plot(f)
+
+# 16. obsprobs <- table(f)/1000
+plot(obsprobs)
+
+# 17.
+tvals <- dnbinom(0:maxz, size=0.9, mu=10)
+points(0:maxz, tvals)
+
+#18.
+# 19. 
+library(dplyr)
+data3 =  filter(data2, GRazing == "Grazed", Fruit <50)
+# 20.
+data4 = data2[order(-data$Fruit), ]
+# arrange(data2, desc(Fruit))
+
+# 21. 
+data4 <- group_by(data2, Grazing)
+data4summary <-summarize(data4, meanFruit = mean(Fruit, na.rm=TRUE))
+
+# 22. 
+data2 %>% arrange(desc(Fruit)) %>% group_by(Grazing) %>%
+
+
+
 
---
+++ BL5233.Practicals.3.R
@@ -0,0 +1,44 @@
+# BL5233: Tutorial 2
+
+# Exercise A 
+
+# Read the dataset “ipomopsis” into R where Fruit is the dependent variable.
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
+data <- read.table(file="ipomopsis.txt", header=TRUE, sep="\t")
+
+# Propose models using the function lm() in which:
+
+# 1. Fit the dependent variable against the main effects only
+model1 <- lm(Fruit ~ Root+Grazing, data=data)
+
+# 2. The same but forcing the model through the origin and log transforming the dependent variable
+model2 <- lm(log(Fruit) ~ Root+Grazing - 1, data=data)
+
+# 3. Fit the main effects and first order interactions
+model3 <- lm(Fruit ~ (Root+Grazing)^2, data=data)
+
+# 4. Fit up to two-way interactions and the quadratic term of Root
+model4 <- lm(Fruit ~ (Root*Grazing) + I(Root^2), data=data)
+
+# 5. Extract the AIC and BIC of the four models. Which model seems more supported?
+modelAIC1 <- AIC(model1)
+modelAIC2 <- AIC(model2)
+modelAIC3 <- AIC(model3)
+modelAIC4 <- AIC(model4)
+
+modelBIC1 <- BIC(model1)
+modelBIC2 <- BIC(model2)
+modelBIC3 <- BIC(model3)
+modelBIC4 <- BIC(model4)
+
+# 6. Produce a summary of each of the models and check what is the value of the 
+# coefficient for Root size. Then do this while putting the models in a list.
+summary(model1)
+summary(model2)
+summary(model3)
+summary(model4)
+
+listModels <- list(model1, model2, model3, model4)
+
+# 7. Extract the coefficients from all models and put them in a list.
+
