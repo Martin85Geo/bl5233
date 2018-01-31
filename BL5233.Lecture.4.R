2ef1dee2811edcd876b9b93e25f79caecaaf124c
dondealban <dondealban@gmail.com>
2018-01-31 13:06:48 +0800

    Saved script of lecture 4

--- BL5233.Lecture.4.R
+++ BL5233.Lecture.4.R
@@ -15,3 +15,19 @@ lines(seq(0,8,0.1), predict(model1,list(tannin=seq(0,8,0.1))))
 # Model checking
 par(mfrow=c(2,2))
 plot(model1)
+
+
+# Piecewise regression
+data2 <- read.table(file="spArea.txt", header=TRUE, sep="\t")
+names(data2)
+plot(log(data2$Species)~log(data2$Area), pch=16, xlab="Area", ylab="Species")
+
+model2<- lm(log(Species)~log(Area),data=data2)
+plot(log(data2$Area),resid(model2))
+
+model2<-lm(log(Species)~(log(Area)*(Area<148))+(log(Area)*(Area>=148)),data=data2)
+area=sort(unique(data2$Area))
+plot(log(data2$Species)~log(data2$Area ))
+lines(log(area), predict(model2, list(Area=area)))
+
+    # Then, compare the two models by using AIC or ANOVA test
