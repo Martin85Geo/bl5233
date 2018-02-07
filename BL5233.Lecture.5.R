6c0d3c7135d2de7cfe5524132e5e2de7f24d953c
dondealban <dondealban@gmail.com>
2018-02-07 13:37:49 +0800

    Saved lecture 5 script

--- BL5233.Lecture.5.R
+++ BL5233.Lecture.5.R
@@ -37,8 +37,40 @@ model3 <- aov(gain~diet+supplement, data=weights)
 summary.lm(model3)
 
 # Contrasts
-
 clip <- read.table(file="clipping.txt", header=TRUE, sep="\t")
 names(clip)
-plot(clip$clipping, clip$biomass ,xlab="Competition treatment", ylab="Biomass")
+plot(clip$clipping, clip$biomass, xlab="Competition Treatment", ylab="Biomass")
+model4 <- aov(biomass~clipping, data = clip)
+summary.lm(model4)
 
+contrasts(clip$clipping) <- cbind(c(4,-1,-1,-1,-1), c(0,1,1,-1,-1), 
+                                  c(0,0,0,1,-1), c(0,1,-1,0,0))
+contrasts(clip$clipping)
+
+model5 <- aov(biomass~clipping, data=clip)
+summary.lm(model5)
+
+# ANOVA for split plots
+yields <- read.table(file="splityield.txt", header=TRUE, sep="\t")
+names(yields)
+model6 <- aov(yield~irrigation*density*fertilizer + Error(block/irrigation/density), data=yields)
+summary(model6)
+interaction.plot(yields$fertilizer, yields$irrigation, yields$yield)
+interaction.plot(yields$density, yields$irrigation, yields$yield)
+
+    # Visualise interactions through plots using package effects or package visreg
+
+# ANCOVA
+regrowth <- read.table(file="ipomopsis.txt", header=TRUE, sep="\t")
+names(regrowth)
+
+plot(regrowth$Root, regrowth$Fruit, pch=16+as.numeric(regrowth$Grazing),
+     col=c("blue","red")[as.numeric(regrowth$Grazing)])
+abline(lm(Fruit[Grazing=="Grazed"]~Root[Grazing=="Grazed"], data=regrowth),lty=2,col= "blue")
+abline(lm(Fruit[Grazing=="Ungrazed"]~Root[Grazing=="Ungrazed"], data=regrowth),lty=2, col="red")
+
+tapply(Fruit,Grazing, mean)
+t.test(regrowth$Fruit~regrowth$Grazing)
+model7 <- lm(Fruit~Grazing*Root, data=regrowth)
+model8 <- update(model7, ~ .- Grazing:Root)
+anova(model7,model8)
