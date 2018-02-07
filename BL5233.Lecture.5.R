7c17771fca467d3d67e4fb6c461bf96edd196255
dondealban <dondealban@gmail.com>
2018-02-07 12:44:31 +0800

    Saved lecture 5 script

---
+++ BL5233.Lecture.5.R
@@ -0,0 +1,14 @@
+# On ANOVA
+
+
+# Linear regression
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
+dataY <- read.table(file="yields.txt", header=TRUE, sep="\t")
+
+# Arrange variables
+y <- c(dataY$sand,dataY$clay,dataY$loam)
+soil <- factor(rep(1:3,c(10,10,10)))
+cbind(y, soil)
+
+# Plot
+plot(soil,y,names=c("sand","clay","loam"), ylab="yield")
