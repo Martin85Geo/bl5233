f3909ded2f85335b41026e223034b79b09c61c8a
dondealban <dondealban@gmail.com>
2018-02-14 12:21:23 +0800

    Saved lecture 7 script

--- BL5233.Lecture.7.R
+++ BL5233.Lecture.7.R
@@ -7,7 +7,17 @@ Rconf <- read.table(file="R confidence.txt", header=TRUE, sep="\t")
 # Plotting in Base R
 plot(Confidence~Month, data=Rconf)
 
+# Other plot settings
 par(mar=c(5.1,4.1,4.1,2.1))
+par(family="serif",font=2)
+plot(Confidence~Month,data=Rconf, col=3, font.axis =2, font.lab = 2)
+par(cex=2)
+par(bty="l")
 
-pa
+par(mar=c(5.1,4.1,4.1,2.1))
+plot(Confidence~Month,data=Rconf,bg="grey", family="serif",cex.lab=1.2,bty="l",pch=15)
 
+# Save plot as pdf file
+pdf("Fig1.pdf")
+plot(Confidence~Month, data=Rconf)
+dev.off()
