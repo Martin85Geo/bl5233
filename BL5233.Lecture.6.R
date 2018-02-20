7775c7df3f1d6634228c1da04074c64f91275441
dondealban <dondealban@gmail.com>
2018-02-20 11:00:17 +0800

    Saved lecture 6 script

--- BL5233.Lecture.6.R
+++ BL5233.Lecture.6.R
@@ -21,3 +21,22 @@ lines(xv, exp(yv))
 # Analysis of deviance
 count <- read.table(file="cells.txt", header=TRUE, sep="\t")
 names(count)
+table(count$cells) # Frequencies in the counts of a variable
+tapply(count$cells, count$smoker, mean)
+
+# Create the model
+model1 <- glm(cells~smoker*sex*age*weight, poisson, data=count)
+summary(model1)
+
+# Since residual deviance is larger that the degrees of freedom, then it is a problem of
+# overdispersion. Hence we need to re-fit the model using quasi-Poisson to account for
+# overdispersion
+model2 <- glm(cells~smoker*sex*age*weight, quasipoisson, data=count)
+summary(model2)
+
+# Remove the most complicated terms by step-wise simplification approach
+model3 <- update(model2,~. -smoker:sex:age:weight) # 4-way interaction
+anova(model2, model3, test="F")
+
+model4 <- update(model3,~. - sex:age:weight) # 3-way interaction
+anova(model3, model4, test="F")
