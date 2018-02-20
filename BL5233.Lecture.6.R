0694e3411a7a27b51413b08439b222a2348d463b
dondealban <dondealban@gmail.com>
2018-02-20 10:43:43 +0800

    Saved lecture 6 script

---
+++ BL5233.Lecture.6.R
@@ -0,0 +1,8 @@
+# On Generalised Linear Models
+
+# Prepare data
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
+clusters <- read.table(file="clusters.txt", header=TRUE, sep="\t")
+
+names(clusters)
+
