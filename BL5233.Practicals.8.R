e4207b77da904523bedae034270b38815b5da753
dondealban <dondealban@gmail.com>
2018-03-13 10:52:16 +0800

    Saved Practicals 8 script

--- BL5233.Practicals.8.R
+++ BL5233.Practicals.8.R
@@ -5,3 +5,13 @@
 # (1 if infected and 0 otherwise). The explanatory variables are the area, year and depth
 # the fish where caught at, the length weight and age of the fish.
 
+# 1. Read the dataset ParasiteCod in. Year and area should be factors, convert the
+# variables. Remove the missing data from the dataset (hint: use na.omit). 
+
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
+data <- read.table(file="ParasiteCod.txt", header=TRUE, sep="\t")
+data$Year <- as.factor(data$Year)
+data$Area <- as.factor(data$Area)
+data <- na.omit(data) # Original 1254 entries reduced to 1191 entries after removing missing data
+
+
