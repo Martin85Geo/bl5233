cd5d07e4b23c8b13587cf85f86f2de9ea06b5fd9
dondealban <dondealban@gmail.com>
2018-03-13 11:08:37 +0800

    Saved Practicals 8 script

--- BL5233.Practicals.8.R
+++ BL5233.Practicals.8.R
@@ -10,8 +10,19 @@
 
 setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
 data <- read.table(file="ParasiteCod.txt", header=TRUE, sep="\t")
+names(data)
 data$Year <- as.factor(data$Year)
 data$Area <- as.factor(data$Area)
 data <- na.omit(data) # Original 1254 entries reduced to 1191 entries after removing missing data
 
+# 2. Some of the variables are highly correlated. Perform a multicollinearity analysis and
+# remove those that are highly correlated (hint: you need to load package “car” and use the
+# function “vif” on a GLM model fitted only to the main effects. Those variables with a 
+# score above 3 should be removed one at a time).
+library(car)
+model0 <- glm(Prevalence ~ Area+Year+Depth+Length+Weight+Age, family=binomial, data=data)
+summary(model0)
+vif(model0) # Check for multicollinearity
 
+model1 <- glm(Prevalence ~ Area+Year+Depth+Weight+Age, family=binomial, data=data) # Remove Length
+model2 <- glm(Prevalence ~ Area+Year+Depth+Age, family=binomial, data=data) # Remove weight
