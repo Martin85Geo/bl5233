a2dcc09b361de389b4890f71858c356303774c4a
dondealban <dondealban@gmail.com>
2018-03-13 12:58:36 +0800

    Saved Practicals 8 script

--- BL5233.Practicals.8.R
+++ BL5233.Practicals.8.R
@@ -23,6 +23,26 @@ library(car)
 model0 <- glm(Prevalence ~ Area+Year+Depth+Length+Weight+Age, family=binomial, data=data)
 summary(model0)
 vif(model0) # Check for multicollinearity
-
 model1 <- glm(Prevalence ~ Area+Year+Depth+Weight+Age, family=binomial, data=data) # Remove Length
-model2 <- glm(Prevalence ~ Area+Year+Depth+Age, family=binomial, data=data) # Remove weight
+summary(model1)
+vif(model1)
+model2 <- glm(Prevalence ~ Area+Year+Depth+Length+Age, family=binomial, data=data) # Remove Weight
+summary(model2)
+vif(model2)
+
+# 3. Fit a GLM model to the data considering the main effects of the variables and the 
+# one-way interaction between area and year.
+model3 <- glm(Prevalence ~ Area*Year+Depth+Weight+Age, family=binomial, data=data) # One-way interaction of
+summary(model3)
+
+# 4. Try to simplify the model.
+anova(model0, model1, test="Chi")
+anova(model0, model2, test="Chi")
+anova(model2, model3, test="Chi")
+
+models <- list(model0, model1, model2, model3)
+lapply(models, anova)
+
+# 5. Fit the same model but now using GAM. Use smoothers to model Depth and Weight.
+
+
