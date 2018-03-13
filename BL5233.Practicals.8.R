6ddd0aa4370bce77be2021f15945765f8ef9a060
dondealban <dondealban@gmail.com>
2018-03-13 10:41:10 +0800

    Saved Practicals 8 script

---
+++ BL5233.Practicals.8.R
@@ -0,0 +1,7 @@
+# BL5233: Tutorial 6
+
+# Exercise A. Let’s look at a presence-absence type dataset. The example looks at the 
+# presence of a trypanosome blood parasite in cod. The dependent variable is “Prevalence”
+# (1 if infected and 0 otherwise). The explanatory variables are the area, year and depth
+# the fish where caught at, the length weight and age of the fish.
+
