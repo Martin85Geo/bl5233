628dead6b801bcee559279cee00a02ec1fc5a51d
dondealban <dondealban@gmail.com>
2018-03-20 10:47:04 +0800

    Saved Practicals 9 script

--- BL5233.Practicals.9.R
+++ BL5233.Practicals.9.R
@@ -1,48 +1,15 @@
-# BL5233: Tutorial 6
+# BL5233: Tutorial 7
 
-# Exercise A. Let’s look at a presence-absence type dataset. The example looks at the 
-# presence of a trypanosome blood parasite in cod. The dependent variable is “Prevalence”
-# (1 if infected and 0 otherwise). The explanatory variables are the area, year and depth
-# the fish where caught at, the length weight and age of the fish.
-
-# 1. Read the dataset ParasiteCod in. Year and area should be factors, convert the
-# variables. Remove the missing data from the dataset (hint: use na.omit). 
+# Exercise A. The study looks at the number of follicles of more than 10mm of diameter in 
+# the ovaries of 11 mares observed repeatedly through time. The time has been scaled so
+# that ovulation occurs at time 0 and 1. Previous studies show that the relationship 
+# between the number of follicles (y) and time (t) is well modeled as:
+# y =a+bsin(2pt)+dcos(2pt)+eij
+# This dataset has an added difficulty. The measures are not expected to be independent 
+# between mares. We are going to jump ahead a bit and use also random effects to indicate 
+# the nested structure (this is pretty similar to what we were doing with split-plot 
+# ANOVAs). 
 
 setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
 data <- read.table(file="ParasiteCod.txt", header=TRUE, sep="\t")
 names(data)
-data$Year <- as.factor(data$Year)
-data$Area <- as.factor(data$Area)
-data <- na.omit(data) # Original 1254 entries reduced to 1191 entries after removing missing data
-
-# 2. Some of the variables are highly correlated. Perform a multicollinearity analysis and
-# remove those that are highly correlated (hint: you need to load package “car” and use the
-# function “vif” on a GLM model fitted only to the main effects. Those variables with a 
-# score above 3 should be removed one at a time).
-library(car)
-model0 <- glm(Prevalence ~ Area+Year+Depth+Length+Weight+Age, family=binomial, data=data)
-summary(model0)
-vif(model0) # Check for multicollinearity
-model1 <- glm(Prevalence ~ Area+Year+Depth+Weight+Age, family=binomial, data=data) # Remove Length
-summary(model1)
-vif(model1)
-model2 <- glm(Prevalence ~ Area+Year+Depth+Length+Age, family=binomial, data=data) # Remove Weight
-summary(model2)
-vif(model2)
-
-# 3. Fit a GLM model to the data considering the main effects of the variables and the 
-# one-way interaction between area and year.
-model3 <- glm(Prevalence ~ Area*Year+Depth+Weight+Age, family=binomial, data=data) # One-way interaction of
-summary(model3)
-
-# 4. Try to simplify the model.
-anova(model0, model1, test="Chi")
-anova(model0, model2, test="Chi")
-anova(model2, model3, test="Chi")
-
-models <- list(model0, model1, model2, model3)
-lapply(models, anova)
-
-# 5. Fit the same model but now using GAM. Use smoothers to model Depth and Weight.
-
-
