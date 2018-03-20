fc6bec934b3ed851c8115726c038d8962c38bffe
dondealban <dondealban@gmail.com>
2018-03-20 11:47:59 +0800

    Saved Practicals 9 script

--- BL5233.Practicals.9.R
+++ BL5233.Practicals.9.R
@@ -29,7 +29,7 @@ xyplot(follicles ~ Time, data=data, groups=factor(Mare), xlab="Time", ylab="Foll
 
 library(nlme)
 model0 <- lme(follicles ~ sin(2*pi*Time) + cos(2*pi*Time), data=data, random=~1|Mare)
-summary(model0)
+summary(model0) # AIC=1669.36
 
 # 3. Investigate the autocorrelation structure of the residuals of the model just fitted
 # and plot it.
@@ -40,6 +40,12 @@ plot(data$Time[I], E, ylab="residuals")
 plot(ACF(model0), alpha=0.05)
 
 # Note: the ACF plot indicates there is significant correlation for a lag of 1
+
+model1 <- lme(follicles ~ sin(2*pi*Time) + cos(2*pi*Time), data=data, random=~1|Mare, correlation=corAR1(form=~Time))
+summary(model1) # AIC=1602.387
+
+# Hence, model1 is improvement from model0 due to lower AIC
+# Also, phi1=0.5929379
 
 # 4. Fit different models considering different ARMA correlation structures with 
 # autoregression and moving average coefficients ranging from 0 to 2. Compare the models
