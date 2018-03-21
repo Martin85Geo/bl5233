b503565e38a8bf3f66d69ee72d4dbf238613dd33
dondealban <dondealban@gmail.com>
2018-03-21 14:00:58 +0800

    Saved Lecture 10 script

--- BL5233.Lecture.10.R
+++ BL5233.Lecture.10.R
@@ -85,6 +85,105 @@ mydata <- data.frame(E,SDI2003$Easting, SDI2003$Northing)
 coordinates(mydata)<- c("SDI2003.Easting","SDI2003.Northing")
 bubble(mydata,"E",col=c("black","grey"), main="Normalised residuals", xlab="X-coordinates",ylab="Y-coordinates")
 
-# Continue example at home...
+# Spatial autocorrelation using GLMs
+
+require(MASS)
+require(nlme)
+data1 <- read.table("data.txt", head=T)
+# define a grouping factor that assigns all observations to the same group
+group <- factor(rep("a",nrow(data1)))
+data1 <- cbind(data1, group)
+# The data have to be attached AND specified in the formula.
+attach(data1)
+# Exponential correlation structure (the others also work)
+model.e <- glmmPQL(y ~ x + z, random=~1|group, data=data1, correlation=corExp(form=~lat+long), family=binomial)
+
+
+# Linear mixed-effects models I
+
+# Random Intercept Model
+library(nlme)
+RIKZ <- read.table(file="RIKZ.txt", header=TRUE, sep="\t")
+names(RIKZ)
+
+RIKZ$fBeach <- factor(RIKZ$Beach)
+Mlme1 <- lme(Richness ~ NAP, random=~1 | fBeach, data=RIKZ)
+summary(Mlme1)
+
+F0 <- fitted(Mlme1,level=0)
+F1 <- fitted(Mlme1,level=1)
+I <- order(RIKZ$NAP)
+NAPs <- sort(RIKZ$NAP)
+plot(NAPs,F0[I], lwd=4, type="l",  ylim=c(0,22), ylab="Richness",xlab="NAP")
+for (i in 1:9){
+  x1 <- RIKZ$NAP[RIKZ$Beach==i]
+  y1 <- F1[RIKZ$Beach==i]
+  K <- order(x1)
+  lines(sort(x1),y1[K])
+}
+text(RIKZ$NAP,RIKZ$Richness,RIKZ$Beach,cex=0.9)
+
+# Random Intercept and Slope Model
+Mlme2 <- lme(Richness ~ NAP, random = ~1 + NAP | fBeach, data = RIKZ)
+summary(Mlme2)
+
+F0 <- fitted(Mlme2,level=0)
+F1 <- fitted(Mlme2,level=1)
+I <- order(RIKZ$NAP)
+NAPs <- sort(RIKZ$NAP)
+plot(NAPs,F0[I], lwd=4, type="l",  ylim=c(0,22), ylab="Richness",xlab="NAP")
+for (i in 1:9){
+  x1 <- RIKZ$NAP[RIKZ$Beach==i]
+  y1 <- F1[RIKZ$Beach==i]
+  K <- order(x1)
+  lines(sort(x1),y1[K])
+}
+text(RIKZ$NAP,RIKZ$Richness,RIKZ$Beach,cex=0.9)
+
+# Random Effects Model
+Mlme3 <- lme(Richness ~ 1, random = ~1 | fBeach, data = RIKZ)
+summary(Mlme3)
+
+# Diagnostic plots
+plot(Mlme2)
+plot(Mlme2, resid(., scaled=TRUE) ~ fitted(.), abline = 0)
+
+# Diagnostic plot to check for heterodeodasticity
+plot(Mlme2, resid(., scaled=TRUE) ~ fitted(.) | fBeach, abline = 0)
+
+# Diagnostics for residuals
+plot(Mlme2, fBeach ~ resid(., scaled=TRUE))
+plot(Mlme2, resid(., scaled=TRUE) ~ NAP | fBeach, abline = 0)
+
+# Check normality
+qqnorm(Mlme2, ~ resid(., type = "p") | fBeach, abline = c(0, 1))
+
+# Check assumption of normality of the random effects
+qqnorm(Mlme2, ~ranef(.))
+plot(ranef(Mlme2))
+
+#Compare with AIC
+AIC(Mlme1, Mlme2, Mlme3)
+
+# Plot standard errors
+library(lme4)
+Mlme2 <- lmer(Richness ~ NAP + (NAP | fBeach), data = RIKZ)
+library(lattice) 
+reAndSE <- ranef(Mlme2, condVar = TRUE) 
+dotplot(reAndSE)
+
+#R2 in mixed-effects model
+library(MuMIn)
+r.squaredGLMM(Mlme1)
+# The random intercept model explains 61% of the variance and 27% is explained by the 
+# fixed effects alone.
+
+# Models with multiple non-nested random effects
+library(lme4)
+earnings <- read.table(file="earnings2.csv", header=TRUE, sep=",")
+earnings$ageF = factor(earnings$age)
+earnings$ethF = factor(earnings$eth)
+M1 <- lmer (y ~ x.centered + (1 + x.centered | ethF) + (1 + x.centered | ageF) + (1 + x.centered | ethF:ageF), data=earnings)
+M1
 
 
