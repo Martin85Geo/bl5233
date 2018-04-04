b36ac4b51cd2a4fc4ea4b13fd54d156fe66d65b9
dondealban <dondealban@gmail.com>
2018-04-04 12:36:25 +0800

    Saved Lecture 12 script

---
+++ BL5233.Lecture.12.R
@@ -0,0 +1,58 @@
+# GLMM
+
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
+# Squid <- read.table(file="Squid.txt", header=TRUE, sep="\t")
+# names(Squid)
+
+# Bacteria example with binary data
+library(MASS)
+data(bacteria)
+names(bacteria)
+
+bacteria$y <- 1*(bacteria$y=="y")
+
+library(lme4)
+model1 <- glmer(y~trt+(week|ID), family=binomial, nAGQ=1, data=bacteria)
+summary(model1)
+
+model2 <- glmer(y~trt+(1|ID), family=binomial, nAGQ=1, data=bacteria)
+anova(model1, model2)
+
+model3 <- glmer(y~1+(week|ID), family=binomial, nAGQ=1, data=bacteria)
+anova(model1, model3)
+
+# Deer example
+DeerEcervi <- read.table(file="DeerEcervi.txt", header=TRUE, sep="\t")
+names(DeerEcervi)
+
+#convert Ecervi into binary variable
+DeerEcervi$Ecervi.01 <- DeerEcervi$Ecervi
+DeerEcervi$Ecervi.01[DeerEcervi$Ecervi>0] <- 1
+#convert sex and farm into factors
+DeerEcervi$fSex <- factor(DeerEcervi$Sex)
+DeerEcervi$fFarm <- factor(DeerEcervi$Farm)
+#center length
+DeerEcervi$CLength <- DeerEcervi$Length -
+mean(DeerEcervi$Length)
+
+DE.PQL <- glmmPQL(Ecervi.01 ~ CLength * fSex, random=~1|fFarm, family=binomial, data=DeerEcervi)
+summary(DE.PQL)
+(DE.PQL$sigma)^2
+
+library(lme4)
+DE.lme4 <- glmer(Ecervi.01 ~ CLength * fSex +(1|fFarm), family=binomial, data=DeerEcervi)
+summary(DE.lme4)
+
+install.packages("glmmML")
+library(glmmML)
+DE.glmmML<-glmmML(Ecervi.01 ~ CLength * fSex, cluster = fFarm, family=binomial, data = DeerEcervi)
+summary(DE.glmmML)
+
+# Homework!
+# Information-theoretic model selection: koalas in fragmented landscapes
+
+
+# MCMC
+
+
+
