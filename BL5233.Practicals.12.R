eeaecf1b22abeaa7c513558be80c42a84234b335
dondealban <dondealban@gmail.com>
2018-04-10 08:51:52 +0800

    Saved Practicals 12 script

---
+++ BL5233.Practicals.12.R
@@ -0,0 +1,8 @@
+# BL5233: Tutorial 10
+
+# Exercise A. The data used in this exercise comprise statistical characterizations of the
+# vocalizations of 234 individual birds of the species Macropygia amboinensis (the Brown 
+# Cuckoo-Dove; thanks to Frank Rheindt for the data and Ryan Chisholm for the exercise). 
+# The goal of the exercise is to see whether the individual birds can be separated into 
+# groups based on the characteristics of their vocalizations.
+
