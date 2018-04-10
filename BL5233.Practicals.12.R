382e29bce1d1e97ade77e828e1ef273063f4be69
dondealban <dondealban@gmail.com>
2018-04-10 11:01:20 +0800

    Saved Practicals 12 script

--- BL5233.Practicals.12.R
+++ BL5233.Practicals.12.R
@@ -6,6 +6,7 @@
 # The goal of the exercise is to see whether the individual birds can be separated into 
 # groups based on the characteristics of their vocalizations.
 
+setwd("/Users/dondealban/Desktop/BL5233/Datasets/")
 data <- read.table(file="vocalisation_data.txt", header=TRUE, sep="\t")
 names(data)
 
@@ -13,3 +14,15 @@ names(data)
 # is also tab delimited). What proportion of the variance is explained by the first 
 # principal component? How many principal components do you need to explain ~ 95% of the 
 # variance?
+
+model0 <- prcomp(data, scale=T)
+plot(model0$x[,1], model0$x[,2], pch=19, xlab="PC1", ylab= "PC2")
+summary(model0)
+
+# PC1 explains 31.3% of the variance. Five principal components (up to PC5) needed to 
+# explain 99.7% of the variance.
+
+# 2. Make a scree plot.
+
+plot(model0)
+biplot(model0)
